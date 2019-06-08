19f1b563041aadc750a379f80cb5919c08c04c20
Seetha71098 <42378980+Seetha71098@users.noreply.github.com>
2019-06-08 11:53:33 +0530

    Create PosNeg.java

---
+++ PosNeg.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PosNeg {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number:");
+		int n=scan.nextInt();
+		if(n>0){
+			System.out.println("positive number");
+		}
+		else if(n<0){
+			System.out.println("negative number");
+		}
+		else{
+			System.out.println("even");
+		}
+		
+	}
+
+}
