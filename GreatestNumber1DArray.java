f9eed0e3534dcc8c94580a2ddfc84ab002ae3987
Seetha71098 <42378980+Seetha71098@users.noreply.github.com>
2019-06-20 12:18:32 +0530

    Create GreatestNumber1DArray.java

---
+++ GreatestNumber1DArray.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class GreatestNumber1DArray {
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=scan.nextInt();
+			}
+		int max=arr[0];
+	for(int i=0;i<n;i++) {
+			if(arr[i]>max) {
+					max=arr[i];
+					}
+				}
+		System.out.println(max);
+		scan.close();
+		}
+	}
