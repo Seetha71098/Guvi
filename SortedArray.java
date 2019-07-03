ecc5f6d45d67ae8514bae3db00e60b741e711df4
Seetha71098 <42378980+Seetha71098@users.noreply.github.com>
2019-07-03 12:00:54 +0530

    Create SortedArray.java

---
+++ SortedArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SortedArray {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int [] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=scan.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++){
+			System.out.print(arr[i] +" ");
+		}
+		scan.close();
+	}
+}
