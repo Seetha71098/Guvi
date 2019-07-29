e0522b4d63ee93fbbb9e7d0a1c93ee9d475a3385
Seetha71098 <42378980+Seetha71098@users.noreply.github.com>
2019-07-29 14:03:10 +0530

    Update and rename SumOfSquareOfDigit.java to SumofSquareOfDigit.java

---
+++ SumofSquareOfDigit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SumofSquareOfDigit {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int sum=0,rev=0;
+		int n=scan.nextInt();
+		int num=scan.nextInt();
+		for(int i=0;i<n;i++){
+			rev=num%10;
+			sum=sum+(rev*rev);
+			num=num/10;
+		}
+		System.out.println(sum);
+		scan.close();
+	}
+}
